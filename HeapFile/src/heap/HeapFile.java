f4085605be95221a9dbcb65040125fe0d9ad90d0
Hyun Chai Jeong <jeong3@purdue.edu>
2015-10-22 17:47:20 -0400

    worked little on part 2,3

--- HeapFile/src/heap/HeapFile.java
+++ HeapFile/src/heap/HeapFile.java
@@ -4,22 +4,132 @@ import global.Convert;
 import global.GlobalConst;
 import global.Minibase;
 import global.RID;
+import global.Page;
+import global.PageId;
 
-import java.io.IOException;
+import java.io.*;
 
 import chainexception.ChainException;
 
 public class HeapFile {
 
-	String name;
-	int numRecord;
+	/*
+		Hash table of <RID, Tuple> pairs.
+	*/
+	public class record {
+		RID rid;
+		Tuple tuple;
 
+		public record() {
+			rid = null;
+			tuple = null;
+		};
+	}
+
+	/*
+		Return hash value of this rid.
+	*/
+	public int hash(RID rid) {
+		return rid.hashCode();
+	}
+
+	/*
+		pick min prime number >= val
+	*/
+	public int getPrime(int val) {
+		int minPrime = 0;
+		int ret = val;
+
+		while(true) {
+			boolean isPrime = true;
+
+			for (int j=2; j<ret; j++) {
+				if (ret % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+
+			if (isPrime) {
+				return ret;
+			}
+
+			ret++;
+		}
+	}
+
+	/*
+		Page directory class.
+	*/
+	public class directory {
+
+		HFPage[] pageLocation;
+		int[] free_space;
+
+		public directory() {
+			pageLocation = new HFPage[numPages];
+			free_space = new int[numPages];
+
+			for (int i=0; i<numPages; i++) {
+				pageLocation[i] = null;
+				free_space[i] = dummy.getFreeSpace();
+			}
+		};
+	}
+
+
+	int numRecord;
+	int numPages;
+	int numDir;
+
+	directory[] dir;
+	record[] records;
+	int htsize;
+	int a;
+	int b;
+
+	HFPage dummy = new HFPage();
+	PageId header;
+	Page headerPage;
 
 	/*
 		Constructor.
 	*/
 	public HeapFile(String name) {
-		this.name = name;
+
+		// try to open HeapFile
+		PageId file = Minibase.DiskManager.get_file_entry(name);
+
+		header = Minibase.DiskManager.allocate_page();
+		headerPage = new Page();
+		Minibase.DiskManager.read_page(header, headerPage);
+
+		// no such HeapFile exists; create an empty file
+		if (file == null) {
+System.out.println("file empty; create a new file");
+	
+			Minibase.DiskManager.add_file_entry(name, header);
+
+System.out.println("header pid: " + header.pid);
+
+		//Minibase.DiskManager.write_page(p, header);
+		Minibase.DiskManager.print_space_map();
+		}
+
+		// page directory
+		numDir = 100;
+		numPages = 100;
+		dir = new directory[numDir];
+		for (int i=0; i<numDir; i++) {
+			dir[i] = new directory();
+		}
+
+		// hash table of <RID, Tuple>
+		records = new record[getPrime(numDir*numPages*numPages)];
+		for (int i=0; i<records.length; i++) {
+			records[i] = new record();
+		}
+
 		numRecord = 0;
 	};
 
@@ -29,7 +139,71 @@ public class HeapFile {
 	*/
 	public RID insertRecord(byte[] record)
 		throws SpaceNotAvailableException {
-		numRecord++;
+		
+		// first, look for free space in existing pages
+		for (int i=0; i<numDir; i++) {
+			if (dir[i] != null) {
+
+				for (int j=0; j<numPages; j++) {
+					if (dir[i].pageLocation[j] != null) {
+						if (dir[i].free_space[j] > record.length) {
+
+							//	insert record
+							RID rid;
+							rid = dir[i].pageLocation[j].insertRecord(record);
+							numRecord++;
+
+							// update free space
+							dir[i].free_space[j] = dir[i].pageLocation[j].getFreeSpace();
+
+							// create tuple
+							Tuple tuple = new Tuple(record, 0, record.length);
+							int hash_index = hash(rid);
+							records[hash_index].rid = rid;
+							records[hash_index].tuple = tuple;
+
+							return rid;
+						}
+					}
+				}
+			}
+		}
+
+		// otherwise, allocate a new HFPage and insert record
+		for (int i=0; i<numDir; i++) {
+			if (dir[i] != null) {
+
+				for (int j=0; j<numPages; j++) {
+					if (dir[i].pageLocation[j] == null) {
+
+						// allocate new HFPage
+						HFPage hfp = new HFPage();
+						dir[i].pageLocation[j] = hfp;
+						PageId pid = new PageId(numDir*i + j);
+						hfp.setCurPage(pid);
+//System.out.println("inserting in new HFPage " + hfp.getCurPage());
+//String s = String.valueOf(hfp);
+//System.out.println("addr: " + s);
+
+						// insert record
+						RID rid;
+						rid = hfp.insertRecord(record);
+						numRecord++;
+
+						// update free space
+						dir[i].free_space[j] = hfp.getFreeSpace();
+
+						// create tuple
+						Tuple tuple = new Tuple(record, 0, record.length);
+						int hash_index = hash(rid);
+
+						records[hash_index].rid = rid;
+						records[hash_index].tuple = tuple;
+						return rid;
+					}
+				}
+			}
+		}
 
 		return null;
 	};
@@ -39,7 +213,8 @@ public class HeapFile {
 		return a record.
 	*/
 	public Tuple getRecord(RID rid) {
-		return null;
+		int hashIndex = hash(rid);
+		return records[hashIndex].tuple;
 	};
 
 	
@@ -48,7 +223,106 @@ public class HeapFile {
 	*/
 	public boolean updateRecord(RID rid, Tuple newRecord)
 		throws InvalidUpdateException {
-		
+
+		// find the record
+		PageId pageno = rid.pageno;
+		int slotno = rid.slotno;
+
+System.out.println("Trying to update " + pageno + ", " + slotno);
+
+		int hashIndex = hash(rid);
+		Tuple tuple = records[hashIndex].tuple;
+
+		int dirIndex = pageno.pid / numDir;
+		int pageIndex = pageno.pid % numDir;
+		HFPage hfp = dir[dirIndex].pageLocation[pageIndex];
+
+		// if previous record length > newRecord.length
+		// update in the same page
+		if (tuple.getTupleByteArray().length > newRecord.getTupleByteArray().length) {
+			hfp.updateRecord(rid, newRecord);
+			return true;
+		}
+
+		// if free_space < newRecord.length
+		// delete current record and insert in another page
+		else if (dir[dirIndex].free_space[pageIndex] <= (newRecord.getTupleByteArray().length - tuple.getTupleByteArray().length)) {
+			hfp.deleteRecord(rid);
+
+			// find new place to insert:
+			// first, look for free space in existing pages
+			for (int i=0; i<numDir; i++) {
+				if (dir[i] != null) {
+
+					for (int j=0; j<numPages; j++) {
+						if (dir[i].pageLocation[j] != null) {
+							if (dir[i].free_space[j] > newRecord.getTupleByteArray().length) {
+
+								//	insert record
+								RID tempRid;
+								tempRid = dir[i].pageLocation[j].insertRecord(newRecord.getTupleByteArray());
+
+								// update free space
+								dir[i].free_space[j] = dir[i].pageLocation[j].getFreeSpace();
+
+								// update tuple
+								int hIndex = hash(tempRid);
+								records[hIndex].rid = tempRid;
+								records[hIndex].tuple = newRecord;
+
+								// remove previous hash entry
+								int prevHashIndex = hash(rid);
+								records[prevHashIndex].rid = null;
+								records[prevHashIndex].tuple = null;
+
+								rid = tempRid;
+
+								return true;
+							}
+						}
+					}
+				}
+			}
+
+			// otherwise, allocate a new HFPage and insert record
+			for (int i=0; i<numDir; i++) {
+				if (dir[i] != null) {
+
+					for (int j=0; j<numPages; j++) {
+						if (dir[i].pageLocation[j] == null) {
+
+							// allocate new HFPage
+							HFPage h2 = new HFPage();
+							dir[i].pageLocation[j] = h2;
+							PageId pid = new PageId(numDir*i + j);
+							h2.setCurPage(pid);
+
+							// insert record
+							RID tempRid;
+							tempRid = h2.insertRecord(newRecord.getTupleByteArray());
+
+							// update free space
+							dir[i].free_space[j] = h2.getFreeSpace();
+
+							// update tuple
+							int hIndex = hash(tempRid);
+							records[hIndex].rid = tempRid;
+							records[hIndex].tuple = newRecord;
+
+							// remove previous hash entry
+							int pHashIndex = hash(rid);
+							records[pHashIndex].rid = null;
+							records[pHashIndex].tuple = null;
+
+							rid = tempRid;
+
+							return true;
+						}
+					}
+				}
+			}
+		}
+
 		return false;
 	};
 
@@ -57,9 +331,30 @@ public class HeapFile {
 		delete a record.
 	*/
 	public boolean deleteRecord(RID rid) {
+
+		// find the record
+		PageId pageno = rid.pageno;
+		int slotno = rid.slotno;
+
+System.out.println("Trying to delete " + pageno + ", " + slotno);
+
+		int dirIndex = pageno.pid / numDir;
+		int pageIndex = pageno.pid % numDir;
+		HFPage hfp = dir[dirIndex].pageLocation[pageIndex];
+
+		hfp.deleteRecord(rid);
 		numRecord--;
 
-		return false;
+		// update free space
+		dir[dirIndex].free_space[pageIndex] = hfp.getFreeSpace();
+
+		// if no record left in this page, remove the page
+		if (hfp.firstRecord() == null) {
+			dir[dirIndex].pageLocation[pageIndex] = null;
+			dir[dirIndex].free_space[pageIndex] = dummy.getFreeSpace();
+		}
+
+		return true;
 	};
 
 
@@ -78,4 +373,20 @@ public class HeapFile {
 		HeapScan hs = new HeapScan(this);
 		return hs;
 	};
+
+
+	/*
+		get header pageid.
+	*/
+	public PageId getHeader() {
+		return header;
+	}
+
+
+	/*
+		get header page.
+	*/
+	public Page getHeaderPage() {
+		return headerPage;
+	}
 }
--- HeapFile/src/heap/HeapScan.java
+++ HeapFile/src/heap/HeapScan.java
@@ -4,6 +4,8 @@ import global.Convert;
 import global.GlobalConst;
 import global.Minibase;
 import global.RID;
+import global.Page;
+import global.PageId;
 
 import java.io.IOException;
 
@@ -17,23 +19,67 @@ public class HeapScan {
 
 	HeapFile hf;
 
+	/*
+		Constructor.
+	*/
 	protected HeapScan(HeapFile hf) {
 		this.hf = hf;
+
+		// pin directory header page
+		Minibase.BufferManager.pinPage(hf.getHeader(), hf.getHeaderPage(), false);
+
+		// initialize iterator fields
 	};
 
+	
+	/*
+		close HeapScan if it's open.
+	*/
 	protected void finalize()
 		throws Throwable {
+//System.out.println("finalize");
 	};
 
+
+	/*
+		check if more records left to scan.
+	*/
 	public boolean hasNext() {
-		return false;
+
+		boolean none = false;
+
+		return none;
 	};
 
+
+	/*
+		get next record.
+	*/
 	public Tuple getNext(RID rid) {
+
+		// find the record
+		PageId pageno = rid.pageno;
+		int slotno = rid.slotno;
+
+		int dirIndex = pageno.pid / hf.numDir;
+		int pageIndex = pageno.pid % hf.numDir;
+//		HFPage hfp = hf.dir[dirIndex].pageLocation[pageIndex];
+
+//		boolean next = hfp.hasNext(rid);
+		boolean next = false;
+		if (next == false) {
+			Minibase.BufferManager.unpinPage(hf.getHeader(), false);
+		}
+
 		return null;
 	}
 
+
+	/*
+		unpin any pinned pages.
+	*/
 	public void close()
 		throws ChainException {
+//System.out.println("closing");
 	};
 }
